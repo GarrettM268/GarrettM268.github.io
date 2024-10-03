71d8c0083631d1eb2858ef6cc19a75d71908b42b
GarrettM268 <Garrett@statuscontrols.com>
2024-10-02 19:58:01 -0600

    fix(HomeView.vue): fix intersection observer for scroll menu.. again

--- src/components/pages/HomeView.vue
+++ src/components/pages/HomeView.vue
@@ -34,19 +34,19 @@
 				</li>
 			</ul>
 		</div>
-		<div id="content" class="max-h-full overflow-auto w-1/2">
-			<section id="about" class="about p-4 font-size-tiny">
-				<div class="pb-10">
+		<div id="content" class="max-h-full overflow-auto w-1/2 flex flex-col gap-20 pb-20">
+			<section id="about" class="about p-4 pb-20 font-size-tiny flex flex-col gap-10">
+				<div class="">
 					I wrote my first lines of code my final year of highschool, when my shool offered its first ever programming class. Fast forward to today, and I am graduating with a Bachelor of Science in computer science from Montana State University. During my journey at MSU, I was exposed to dozens of programming languages, and I ultimately found a passion in web development - I was hooked on the creative process of creating UIs, along with the problem solving challenge that comes with app development.
 				</div>
-				<div class="pb-10">
+				<div class="">
 					After my junior year at Montana State I was fortunate to work a summer internship with Status Controls, a software company based in Washington. There, I developed web applications using VueJS. I worked as part of a small team, where we created custom HTML5 applications that interact with crestron control processors to control audio, video, and other devices in commercial and residential buildings. I was able to learn a lot about the software development process, and I was able to see the impact of my work in the real world. As of fall 2024, I am finishing up the last 6 credits of my degree while working full time as an entry level developer with Status Controls. 
 				</div>
-				<div class="pb-20">
+				<div class="">
 					When Im not at my computer, I spend as much time in the mountains as possible. I love to ski, hunt, camp and backpack, climb both rocks and ice, and pretty much any other pursuit that gets me outside and active.
 				</div>
 			</section>
-			<section id="education" class="pb-20">
+			<section id="education" class="">
 				<Card 
 					:tags="['csci331', 'csci440', 'esof322', 'csci466', 'csci468', 'csci495']" 
 					size="full-width"
@@ -69,7 +69,7 @@
 					</template>
 				</Card>
 			</section>
-			<section id="work-experience" class="flex flex-col gap-10 pb-20">
+			<section id="work-experience" class="flex flex-col gap-10">
 				<Card 
 				:tags="['VueJs', 'HTML', 'CSS', 'JavaScript']" 
 				size="full-width"
@@ -115,27 +115,46 @@ const scrollToSection = (sectionId) => {
 	}
 };
 
+
 const observeSections = () => {
 	const sections = document.querySelectorAll('section');
-    const observer = new IntersectionObserver(
-        (entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    currentSection.value = entry.target.id;
-                }
-            });
-        },
-        {
-            rootMargin: '75% 0px 0px 0px',
-            threshold: 0.2 // Adjust threshold as needed
-        }
-    );
 	
-	sections.forEach(section => observer.observe(section));
-		
-	return () => {
-		sections.forEach(section => observer.unobserve(section));
+	const sectionThresholds = {
+		'about': 0.75,
+		'education': 1,
+		'work-experience': .75,
 	};
+
+	const observers = [];
+
+    sections.forEach((section) => {
+        const sectionId = section.id;
+        const threshold = sectionThresholds[section] || 0.5; // Default threshold if not specified
+
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        currentSection.value = entry.target.id;
+                    }
+                });
+            },
+            {
+                root: null, // Use the viewport as the root
+                rootMargin: '0px',
+                threshold: threshold
+            }
+        );
+
+        observer.observe(section);
+        observers.push(observer);
+    });
+
+    return () => {
+        observers.forEach((observer) => {
+            sections.forEach((section) => observer.unobserve(section));
+        });
+    };
 };
 
 onMounted(() => {
